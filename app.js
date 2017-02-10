84be97ccd4493d621b4caef9c475bdd232efa75a
Rajib-Badhon <rajib_badhon@hotmail.com>
2017-02-10 12:22:58 +0600

    js-password

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(".textclass").keyup(function(){
+		var charnumber=$(".textclass").val().length;
+		var remainingchar=60-charnumber;
+
+		if(remainingchar<=-1){
+			$(".limit").text(remainingchar).addClass("red");
+		}
+		else{
+			$(".limit").text(remainingchar).removeClass("red");
+		}
+	});
+});
